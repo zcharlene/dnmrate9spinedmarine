4e22c4216b90cf35351b06fdb18004e98d25fa01
Chaowei (Charlene) Zhang <112230307+zcharlene@users.noreply.github.com>
2023-06-29 17:04:39 +0800

    Array_all.sh

--- Array_all.sh
+++ Array_all.sh
@@ -1,89 +1,51 @@
-## set the input file to process
+# set the input file to process
 name=$(sed -n ${SLURM_ARRAY_TASK_ID}p namelist)
+mom=Mother_SampleID
+dad=Father_SampleID
+ref=REF_GENOME_PATH
 
-module load biokit
 module load gatk
+module load biokit
 
-## read mapping; fixmate; sorted; markduplicate; flagstat; indexing
-bwa mem -t4 -M -R "@RG\tID:{} tSM:{} tPL:BGI tLB:{} tPU:1" ../../ninespine_v7/NSPV7.GCA_902500615.3.fasta ../Raw/${name}_1.fq.gz ../Raw/${name}_2.fq.gz | samtools view -F4 -hb -o ${name}.fq.bam
-chmod o+r ${name}.fq.bam
-samtools fixmate -@ 8 -m ${name}.fq.bam ${name}.fq.fm.bam
-chmod o+r ${name}.fq.fm.bam
-samtools sort -O bam -o ${name}.fq.fm.sorted.bam ${name}.fq.fm.bam
-chmod o+r ${name}.fq.fm.sorted.bam
-samtools index ${name}.fq.fm.sorted.bam ${name}.fq.fm.sorted.bai
-chmod o+r ${name}.fq.fm.sorted.bai
-samtools markdup ${name}.fq.fm.sorted.bam ${name}.fq.fm.sorted.md.bam
-chmod o+r ${name}.fq.fm.sorted.md.bam
-samtools index ${name}.fq.fm.sorted.md.bam ${name}.fq.fm.sorted.md.bai
-chmod o+r ${name}.fq.fm.sorted.md.bai
-samtools flagstat ${name}.fq.fm.sorted.md.bam > ${name}.fq.fm.sorted.md.bam.flagstat
+gatk --java-options "-Xmx15g" CombineGVCFs  \
+   -R ${ref} \
+   --variant ../VCFs/${mom}.g.vcf.gz \
+   --variant ../VCFs/${dad}.g.vcf.gz \
+   --variant ../VCFs/${name}.g.vcf.gz \
+   -O Trio_${name}.g.vcf.gz \
+   --tmp-dir ../../tmp
 
-## rename groups
-picard AddOrReplaceReadGroups \
-I= ${name}.fq.fm.sorted.md.bam \
-O= ${name}.RG.bam \
-ID=${name} \
-SM=${name} \
-LB=${name} \
-PL=DNBseq \
-PU=BGI \
-VALIDATION_STRINGENCY=SILENT
+gatk --java-options "-Xmx15g" GenotypeGVCFs \
+   -R ${ref} \
+   -V Trio_${name}.g.vcf.gz \
+   -O Trio_${name}.vcf.gz \
+   --tmp-dir ../../tmp
 
-chmod ${name}.RG.bam
-
-## Recalibration and filtering
-gatk BaseRecalibrator \
--I ${name}.RG.bam \
--R ../../ninespine_v7/NSPV7.GCA_902500615.3.fasta \
---known-sites ../../ninespine_v7/snps.vcf.gz \
---known-sites ../../ninespine_v7/indels.vcf.gz \
--O ${name}.table
-
-chmod o+r ${name}.table
-
-gatk ApplyBQSR \
--R ../../ninespine_v7/NSPV7.GCA_902500615.3.fasta \
--I ${name}.RG.bam \
---bqsr-recal-file ${name}.table \
---read-filter GoodCigarReadFilter \
---read-filter NotDuplicateReadFilter \
---read-filter PassesVendorQualityCheckReadFilter \
---read-filter MappingQualityReadFilter \
---read-filter MappingQualityAvailableReadFilter \
---read-filter PrimaryLineReadFilter \
---read-filter MappedReadFilter \
--O ${name}.recalibrated.bam
+gatk SelectVariants \
+    -V Trio_${name}.vcf.gz \
+    -select-type SNP \
+    -O Trio_${name}_snp.vcf.gz
 
 
-chmod o+r ${name}.recalibrated.bam
-chmod o+r ${name}.recalibrated.bai
-
-touch coverage.txt
-
-## Flagstats and coverage
-paste <(echo ${name}.recalibrated.bam | sed 's/.recalibrated.bam//g') \
-<(samtools depth ${name}.recalibrated.bam | awk '{sum+=$3; sumsq+=$3*$3} END { print "Average = ",sum/NR; print "Stdev = ",sqrt(sumsq/NR - (sum/NR)**2)}') >> coverage.txt
-
-## CGI bams
-samtools view -b -h -L ../../../ninespine_v7/CGI.bed ${name}.recalibrated.bam > ${name}.CGI.bam
-chmod o+r ${name}.CGI.bam
-samtools index ${name}.CGI.bam ${name}.CGI.bai
-chmod o+r ${name}.CGI.bai
+gatk VariantFiltration \
+    -V Trio_${name}_snp.vcf.gz \
+    -filter "QD < 2.0" --filter-name "QD2" \
+    -filter "SOR > 3.0" --filter-name "SOR3" \
+    -filter "FS > 60.0" --filter-name "FS60" \
+    -filter "MQ < 40.0" --filter-name "MQ40" \
+    -filter "MQRankSum < -12.5" --filter-name "MQRankSum-12.5" \
+       -filter "ReadPosRankSum < -8.0" --filter-name "ReadPosRankSum-8" \
+    -O Trio_${name}_snp_addfilter.vcf.gz
 
 
-## HC
-gatk --java-options "-Xmx8g" HaplotypeCaller \
-         -R ../../ninespine_v7/NSPV7.GCA_902500615.3.fasta \
-         -I ${name}.recalibrated.bam \
-         -O ../VCFs/${name}.g.vcf.gz \
-         -ERC GVCF \
-         -bamout ${name}.bamout.bam \
-         --tmp-dir ../../tmp/ \
-         -A AlleleFraction \
-         -A DepthPerSampleHC \
-         -A Coverage \
-         -A BaseQuality \
-         -A AS_BaseQualityRankSumTest \
-         -A MappingQuality \
-         -A FisherStrand
+gatk SelectVariants \
+    -V Trio_${name}.vcf.gz \
+    -select-type INDEL \
+    -O Trio_${name}_indel.vcf.gz
+
+bcftools view  -i 'FILTER="PASS"' Trio_${name}_snp_addfilter.vcf.gz --regions LG1,LG2,LG3,LG4,LG5,LG6,LG7,LG8,LG9,LG10,LG11,LG12:16900000-33585825,LG13,LG14,LG15,LG16,LG17,LG18,LG19,LG20,LG21,FIN-PYO-0_mitoSeq | bgzip -c > Trio_${name}_ats_snp_filtered.vcf.gz
+tabix -p vcf Trio_${name}_ats_snp_filtered.vcf.gz
+
+zcat Trio_${name}_ats_snp_filtered.vcf.gz | grep -v '\s\./\.' | grep -v '*' |sed 's#|#/#g' > Trio_${name}_snp_nomissing.vcf
+
+POOHA Trio_${name}_snp_nomissing.vcf ${dad} ${mom} ${name} ../BAMs/bam_recal/${name}.recalibrated.bam > ${name}_phase.txt
